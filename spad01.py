cc814be91d92fc5f79224e452d38149fd6526f6f
Axe0lot1 <maksim.prodanovic@gmail.com>
2022-03-06 20:49:30 +0100

    sada radi

--- spad01.py
+++ spad01.py
@@ -10,21 +10,24 @@ import math
 #w=tk.Tk
 #c=tk.Canvas(w, height=1500, width=900, bg="#fff")
 #c.pack
+
+
 def slobodan_pad(x0, v0y, vx):
-    
-    i=0
-    a=0.4
-    vy=v0y-tt*a
-    vy=v0y
-    tf=2*v0y/a
-    yp=0
-    while (tt<tf):
-        tt=0.04*i
-        x=x0+vx*tt
-        y=yp+vy*tt
-        print(x,y) 
-        i=i+1   
-        vy=v0y-tt*a
-        sleep(0.04)
+    tt = 0
+    i = 0
+    a = 0.4
+    vy = v0y
+    tf = 2 * v0y / a
+    yp = 0
+    while tt < tf :
+        tt = 0.04 * i
+        x = x0 + vx * tt
+        y = yp + v0y * tt - a*tt*tt/2 
+        print(f"x = {x:{0}.{5}}, y = {y:{0}.{5}}, vy = {vy}, tf = {tf}, tt = {tt}") 
+        i = i + 1   
+        if y < 0 :
+            break
+#        time.sleep(0.04)
 
 slobodan_pad(5, 20, 8)
+
