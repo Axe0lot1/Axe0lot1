95360aea5c55e2af1f8c5ef8de63af4c05064eb3
Axe0lot1 <maksim.prodanovic@gmail.com>
2022-03-06 14:28:32 +0100

    init push

---
+++ spad01.py
@@ -0,0 +1,28 @@
+
+#imports
+from re import A
+import tkinter as tk
+import time
+import math
+
+#w=tk.Tk
+#c=tk.Canvas(w, height=1500, width=900, bg="#fff")
+#c.pack
+def slobodan_pad(x0, v0y, vx):
+    
+    i=0
+    a=0.4
+    vy=v0y-tt*a
+    vy=v0y
+    tf=2*v0y/a
+    yp=0
+    while (tt<tf):
+        tt=0.04*i
+        x=x0+vx*tt
+        y=yp+vy*tt
+        print(x,y) 
+        i=i+1   
+        vy=v0y-tt*a
+        sleep(0.04)
+
+slobodan_pad(5, 20, 8)
